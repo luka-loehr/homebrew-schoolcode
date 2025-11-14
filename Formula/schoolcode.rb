5ac9d446306af9caea73241a252fa2271844d7a2
test <ycrafty6@gmail.com>
2025-11-14 16:19:49 +0100

    Update SHA256 for v3.0.0

--- Formula/schoolcode.rb
+++ Formula/schoolcode.rb
@@ -8,7 +8,7 @@ class Schoolcode < Formula
   desc "Automated developer tool deployment for macOS Guest accounts"
   homepage "https://github.com/luka-loehr/SchoolCode"
   url "https://github.com/luka-loehr/SchoolCode/archive/refs/tags/v3.0.0.tar.gz"
-  sha256 "" # Will be calculated after creating release
+  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5" # Will be calculated after creating release
   license "Apache-2.0"
   head "https://github.com/luka-loehr/SchoolCode.git", branch: "main"
 
