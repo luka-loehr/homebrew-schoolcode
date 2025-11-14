bfd02f83c444b717a6849dfa3b0c0eb2db5eb6b8
test <ycrafty6@gmail.com>
2025-11-14 16:25:32 +0100

    Use correct repository name: SchoolCode (capital C)

--- Formula/schoolcode.rb
+++ Formula/schoolcode.rb
@@ -6,11 +6,11 @@
 
 class Schoolcode < Formula
   desc "Automated developer tool deployment for macOS Guest accounts"
-  homepage "https://github.com/luka-loehr/schoolcode"
-  url "https://github.com/luka-loehr/schoolcode/archive/refs/tags/v3.0.0.tar.gz"
-  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5" # Will be calculated after creating release
+  homepage "https://github.com/luka-loehr/SchoolCode"
+  url "https://github.com/luka-loehr/SchoolCode/archive/refs/tags/v3.0.0.tar.gz"
+  sha256 "661722ef85c5059b92b86cce1e130ee4d6991dbfe742c3c659baa15ea61092a0" # Will be calculated after creating release
   license "Apache-2.0"
-  head "https://github.com/luka-loehr/schoolcode.git", branch: "main"
+  head "https://github.com/luka-loehr/SchoolCode.git", branch: "main"
 
   depends_on "bash"
 
