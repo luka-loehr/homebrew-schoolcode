5ecf2d78c295db8a2e4d2f8987ea08afd7ac78e1
test <ycrafty6@gmail.com>
2025-11-14 16:24:05 +0100

    Update to use GitHub Actions generated tarball with correct SHA256

--- Formula/schoolcode.rb
+++ Formula/schoolcode.rb
@@ -7,8 +7,8 @@
 class Schoolcode < Formula
   desc "Automated developer tool deployment for macOS Guest accounts"
   homepage "https://github.com/luka-loehr/schoolcode"
-  url "https://github.com/luka-loehr/schoolcode/releases/download/v3.0.0/schoolcode-3.0.0-fresh.tar.gz"
-  sha256 "e62a370376b85b1e4317d8c861152070e7c8a1fdaecddfb2c4e3463fad8c13c6" # Will be calculated after creating release
+  url "https://github.com/luka-loehr/schoolcode/releases/download/v3.0.0/schoolcode-3.0.0.tar.gz"
+  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5" # Will be calculated after creating release
   license "Apache-2.0"
   head "https://github.com/luka-loehr/schoolcode.git", branch: "main"
 
