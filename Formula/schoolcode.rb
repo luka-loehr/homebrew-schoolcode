04be9cd80347ffce0323558932acd52f8995f0ea
test <ycrafty6@gmail.com>
2025-11-14 16:21:44 +0100

    Update SHA256 for release asset

--- Formula/schoolcode.rb
+++ Formula/schoolcode.rb
@@ -8,7 +8,7 @@ class Schoolcode < Formula
   desc "Automated developer tool deployment for macOS Guest accounts"
   homepage "https://github.com/luka-loehr/schoolcode"
   url "https://github.com/luka-loehr/schoolcode/releases/download/v3.0.0/schoolcode-3.0.0.tar.gz"
-  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5" # Will be calculated after creating release
+  sha256 "3213620f57d4cb2c180519cf48da8e9dcd4a187fb353e06078067d9a2564bd99" # Will be calculated after creating release
   license "Apache-2.0"
   head "https://github.com/luka-loehr/schoolcode.git", branch: "main"
 
